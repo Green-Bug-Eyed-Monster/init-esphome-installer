d895601c69f8bca64419a4a5aeafe00506e0b217
Green-Bug-Eyed-Monster <green@bug-eyed.monster>
2025-08-17 02:14:49 +0800

    Update install-init-esphome.sh

--- install-init-esphome.sh
+++ install-init-esphome.sh
@@ -49,7 +49,7 @@ NC='\033[0m' # No Color
 SEPARATOR="${BRIGHT_YELLOW}================================================================================${NC}"
 
 echo -e "\n$SEPARATOR\n"
-echo -e " ${BOLD}Installation is complete.${NC} The ${LIME_GREEN}init-esphome${NC} command has been installed to:\n"
+echo -e " ${BOLD}Installation is complete.${NC} Your ${LIME_GREEN}init-esphome${NC} command has been installed to:\n"
 echo -e " ${SCRIPT_PATH}\n"
 
 echo -e " You can now run the ${LIME_GREEN}init-esphome${NC} command from any location, to create"
