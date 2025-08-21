eebedfde10740c8007f9eceeb148720e4324848b
Green-Bug-Eyed-Monster <green@bug-eyed.monster>
2025-08-22 01:26:08 +0800

    Update install-init-esphome.sh

--- install-init-esphome.sh
+++ install-init-esphome.sh
@@ -51,8 +51,9 @@ SEPARATOR="${BRIGHT_YELLOW}=====================================================
 echo -e "\n$SEPARATOR\n\n"
 echo -e "  ${BOLD}Installation is complete.${NC}\n"
 echo -e "  Your ${LIME_GREEN}init-esphome${NC} command has been installed to:"
-echo -e "  ${SCRIPT_PATH}\n"
-
+echo -e "  ${SCRIPT_PATH}"
+echo -e
+echo -e
 echo -e "  You can now run the ${LIME_GREEN}init-esphome${NC} command from anywhere. It will:"
 echo -e
 echo -e "    - Create the target folder (if it doesn’t already exist)"
@@ -60,7 +61,7 @@ echo -e "    - Set up everything needed for professional IoT development, includ
 echo -e "        - The ESPHome framework"
 echo -e "        - A pre-configured Visual Studio Code environment"
 echo -e
-
+echo -e
 echo -e "  ${BOLD}Usage:${NC} ${LIME_GREEN}init-esphome${NC} <target-folder>"
 echo -e "\n   👽\n"
 echo -e "\n$SEPARATOR\n"
