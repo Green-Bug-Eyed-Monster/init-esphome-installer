6a772c9a4e1d26a1734b9066493fc35adcd84a79
Green-Bug-Eyed-Monster <green@bug-eyed.monster>
2025-08-22 01:21:19 +0800

    Update install-init-esphome.sh

--- install-init-esphome.sh
+++ install-init-esphome.sh
@@ -59,6 +59,14 @@ echo -e "  the files required to perform professional IOT node development, incl
 echo -e "  the ESPHome component software framework, and a tailored Visual Studio"
 echo -e "  Code environment.\n"
 
+echo -e "You can now run the ${LIME_GREEN}init-esphome${NC} command from anywhere. It will:"
+echo -e
+echo -e "  - Create the target folder (if it doesn’t already exist)"
+echo -e "  - Set up everything needed for professional IoT development, including:"
+echo -e "      - The ESPHome framework"
+echo -e "      - A pre-configured Visual Studio Code environment"
+echo -e
+
 echo -e "  ${BOLD}Usage:${NC} ${LIME_GREEN}init-esphome${NC} <target-folder>"
 echo -e "\n   👽\n"
 echo -e "\n$SEPARATOR\n"
